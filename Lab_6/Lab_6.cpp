3c3883473d2e53a510ab72fa330cac240b5d2d32
cmdax <90183750+cmdax@users.noreply.github.com>
2022-06-13 19:31:06 +0300

    Add files via upload

---
+++ Lab_6/Lab_6.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "Tree.h"
+#include "TreeNode.h"
+
+using namespace std;
+
+int main()
+{
+	Tree firstTree;
+	firstTree.fillTree();
+
+	firstTree.displayTree();
+
+	cout <<"Tree height(indexing starts from 0): " << firstTree.getHeight()<<endl;
+
+	int level;
+	cout << "Enter level(indexing starts from 0): ";
+	cin >> level;
+
+	cout << "Amount of nodes on level " << level << " is: " << firstTree.countNodesOnLevel(level)<<endl;
+
+	system("pause");
+}
